9619dfd1a53d64681ccfc68c348c8864be981c87
4bpb <blakebein@gmail.com>
2021-05-19 16:52:55 -0700

    Base stages

---
+++ utils/toHTML.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+var log = require('C:/Users/blake/Documents/GitHub/Chegg-Bot-Request/utils/logger.js')
+
+
+
+let toHTML = function(file, data){
+    fs.writeFile(file, data, function (err) {
+        if (err){
+            log(err,'err')
+        }
+        log('Wrote '+file,'ok')
+      })
+}
+
+
+module.exports = toHTML;
